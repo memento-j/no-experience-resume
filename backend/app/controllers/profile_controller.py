61662ce142eae092544d1e39eb7ccc14e5bae65a
jsales2 <jsales0204@gmail.com>
2025-12-13 19:21:31 -0500

    profile update route  and schema

--- backend/app/controllers/profile_controller.py
+++ backend/app/controllers/profile_controller.py
@@ -1,6 +1,19 @@
 #but if i stop, i'll never be good
 from app.config import supabase
+from schemas.profile_update_schema import ProfileUpdate
 
 def get_profile(user):
     response = supabase.table("profiles").select("*").eq("id", user.id).single().execute()
+    return response.data
+
+def update_profile(profile_data: ProfileUpdate, user):
+    update_data = profile_data.model_dump(exclude_none=True)
+
+    response = (
+        supabase.table("profilees")
+        .update(update_data)
+        .eq("id", user.id)
+        .execute()
+    )
+    
     return response.data
--- backend/app/routes/v1/profile_routes.py
+++ backend/app/routes/v1/profile_routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from app.controllers.profile_controller import get_profile
+from app.controllers.profile_controller import get_profile, update_profile
 from app.dependencies import get_current_user
 
 router = APIRouter()
@@ -7,3 +7,7 @@ router = APIRouter()
 @router.get("/")
 def route_get_profile(user = Depends(get_current_user)):
     return get_profile(user)
+
+@router.patch("/")
+def route_update_profile(payload: ProfileUpdate, user=Depends(get_current_user)):
+    return update_profile(user, payload)
